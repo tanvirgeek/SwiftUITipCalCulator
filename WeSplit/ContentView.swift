88f5d3d1b11efd644105f6dd370f23c3d64491c3
tanvir <tanvirgeek@gmail.com>
2020-08-30 17:44:36 +0600

    This is my first SwiftUI App

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,8 +9,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var people = 2
+    @State private var tipPercentage = 2
+    var tipPercentages = [0,10,15,20,25]
+    var totalPerPerson : Double {
+        // calcutaion
+        let peopleCount = Double(people)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = (tipSelection * orderAmount) / 100
+        let grandTotal = orderAmount + tipValue
+        let perPerson = grandTotal / peopleCount
+        return perPerson
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $people){
+                        ForEach(0..<100){
+                            Text("\($0) number of people")
+                        }
+                    }
+                }
+                
+                Section(header: Text("How much tip do you want to give?")){
+                    Picker("Tip Percentage", selection: $tipPercentage){
+                        ForEach(0..<tipPercentages.count){
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section{
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+        }
+        
     }
 }
 
